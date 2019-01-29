d305d3620d84ec473ba1963ce10a1d8df12dda88
ositaigwe@pursuit.org <j0h@example.com>
2019-01-29 14:57:25 -0500

    creates dynamic playlist using html, css, js, dom manipulation

---
+++ main.js
@@ -0,0 +1,213 @@
+const playlist = {
+  name: 'Soundtrack Star Trek',
+  description: 'The Musical Soundtrack That Influenced A Generation',
+  songs: [
+    {
+      name: "Ilia's Theme",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Main Title",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=182" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },    
+    {
+      name: "Klingon Battle",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=269" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },      
+    {
+      name: "Total Logic",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=594" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Floating Office",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=819" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "The Enterprise",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=883" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Leaving Drydock",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=1243" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Spock's Arrival",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=1454" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "The Cloud",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=1575" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Vejur Flyover",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=1875" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    }, 
+    {
+      name: "The Force Field",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=2174" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Games",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=2478" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Spock Walk",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=2700" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Inner Workings",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=2960" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "Vejur Speaks",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=2960" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "The Meld",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=3374" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    }, 
+    {
+      name: "A Good Start",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=3563" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    },
+    {
+      name: "End Titles",
+      album: "Star Trek: The Motion Picture",
+      composer: ['Jerry Goldsmith'],
+      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=3710" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
+    }           
+  ]
+};
+
+
+const objectToHTML = (song) => {
+  return `<div class='row mb-4'>
+  <div class='col-3'></div>
+    <div class='col-3 my-auto'>
+      ${song.image1}
+    </div>
+    <div class='col-3 align-self-center'>
+      <p class='mb-0 song-name'>${song.name}</p>
+      <p class='song-artists'>${song.composer}</p>
+      <p class='song-artists'>${song.album}</p>
+    </div>
+    <div class='col-3'></div>
+  </div>`;
+}
+
+
+const render = (playlist) => {
+
+  const title = document.querySelector('h1');
+  title.innerText = playlist.name;
+
+  const desc = document.querySelector('.lead');
+  desc.innerText = playlist.description;
+
+  const song_list = document.querySelector('.song-list');
+
+  let combinedHTML = '';
+
+  for (let i = 0; i < playlist.songs.length; i++) {
+    combinedHTML += objectToHTML(playlist.songs[i]);
+  }
+  song_list.innerHTML = combinedHTML;
+  console.log(combinedHTML);
+}
+
+render(playlist);
+
+
+const input = document.querySelector('.js-input');
+input.addEventListener('input', () => {
+  
+  const newList = {
+    name: 'Soundtrack Star Trek',
+    description: 'The Musical Compositions That Influenced A Generation',
+  }
+
+  let arr = [];
+
+  
+
+  for (let i = 0; i < playlist.songs.length; i++) {
+
+    let sName = playlist.songs[i].name;
+    
+    let lowerC = playlist.songs[i].composer.toString().toLowerCase();
+    let lowerS = sName.toLowerCase();
+    let lowerA = playlist.songs[i].album.toString().toLowerCase();
+    let lowerV = input.value.toLowerCase();
+
+
+    if (lowerS.includes(lowerV) === true) {
+        arr.push(playlist.songs[i]);
+    }
+
+    else if (lowerC.includes(lowerV) || lowerS.includes(lowerV) || lowerA.includes(lowerV)) {
+      arr.push(playlist.songs[i]);
+  }
+
+  }
+
+  newList['name'] = playlist.name;
+  newList['description'] = playlist.description;
+  newList['songs'] = arr;
+
+
+  render(newList);
+
+});
+
