a0a6007b876384cf2d14a9ede5308df3fa10e545
ositaigwe@pursuit.org <j0h@example.com>
2019-02-02 20:10:09 -0500

    changes video display from embedded to modal

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const playlist = {
   name: 'Soundtrack Star Trek',
   description: 'The Musical Soundtrack That Influenced A Generation',
-  songs: [
-    {
+  songs: [{
       name: "Ilia's Theme",
       album: "Star Trek: The Motion Picture",
       composer: ['Jerry Goldsmith'],
-      image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      image1: `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
     },
     {
@@ -15,14 +14,14 @@ const playlist = {
       composer: ['Jerry Goldsmith'],
       image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=182" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
-    },    
+    },
     {
       name: "Klingon Battle",
       album: "Star Trek: The Motion Picture",
       composer: ['Jerry Goldsmith'],
       image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=269" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
-    },      
+    },
     {
       name: "Total Logic",
       album: "Star Trek: The Motion Picture",
@@ -71,7 +70,7 @@ const playlist = {
       composer: ['Jerry Goldsmith'],
       image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=1875" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
-    }, 
+    },
     {
       name: "The Force Field",
       album: "Star Trek: The Motion Picture",
@@ -113,7 +112,7 @@ const playlist = {
       composer: ['Jerry Goldsmith'],
       image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=3374" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
-    }, 
+    },
     {
       name: "A Good Start",
       album: "Star Trek: The Motion Picture",
@@ -127,7 +126,7 @@ const playlist = {
       composer: ['Jerry Goldsmith'],
       image1: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=3710" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
-    }           
+    }
   ]
 };
 
@@ -136,7 +135,33 @@ const objectToHTML = (song) => {
   return `<div class='row mb-4'>
   <div class='col-3'></div>
     <div class='col-3 my-auto'>
-      ${song.image1}
+    <button type="button" class="btn transparent" data-toggle="modal" data-target="#ModalCenter">
+<div class="demo-gallery">
+<div>
+  <div class="video" data-poster="https://cimg.tvgcdn.net/i/2018/07/31/b8831a6c-c64c-487f-854b-5b312fd11862/271c159b42ee4bc98215966f19279928/180731-startrekspock-news.jpg">
+      <img class="img-responsive" src="https://cimg.tvgcdn.net/i/2018/07/31/b8831a6c-c64c-487f-854b-5b312fd11862/271c159b42ee4bc98215966f19279928/180731-startrekspock-news.jpg">
+        <div class="demo-gallery-poster">
+        <img src="https://sachinchoolur.github.io/lightGallery/static/img/play-button.png">
+      </div>
+  </div>
+  </div>
+  </div>
+  </button>
+
+<div class="modal fade" id="ModalCenter" tabindex="-1" role="dialog" aria-labelledby="ModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content transparent">
+      <div class="modal-header" id="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body my-auto mx-auto">
+        ${song.image1}
+      </div>
+    </div>
+  </div>
+</div>
     </div>
     <div class='col-3 align-self-center'>
       <p class='mb-0 song-name'>${song.name}</p>
@@ -172,7 +197,7 @@ render(playlist);
 
 const input = document.querySelector('.js-input');
 input.addEventListener('input', () => {
-  
+
   const newList = {
     name: 'Soundtrack Star Trek',
     description: 'The Musical Compositions That Influenced A Generation',
@@ -180,12 +205,12 @@ input.addEventListener('input', () => {
 
   let arr = [];
 
-  
+
 
   for (let i = 0; i < playlist.songs.length; i++) {
 
     let sName = playlist.songs[i].name;
-    
+
     let lowerC = playlist.songs[i].composer.toString().toLowerCase();
     let lowerS = sName.toLowerCase();
     let lowerA = playlist.songs[i].album.toString().toLowerCase();
@@ -193,12 +218,10 @@ input.addEventListener('input', () => {
 
 
     if (lowerS.includes(lowerV) === true) {
-        arr.push(playlist.songs[i]);
-    }
-
-    else if (lowerC.includes(lowerV) || lowerS.includes(lowerV) || lowerA.includes(lowerV)) {
       arr.push(playlist.songs[i]);
-  }
+    } else if (lowerC.includes(lowerV) || lowerS.includes(lowerV) || lowerA.includes(lowerV)) {
+      arr.push(playlist.songs[i]);
+    }
 
   }
 
@@ -210,4 +233,3 @@ input.addEventListener('input', () => {
   render(newList);
 
 });
-
