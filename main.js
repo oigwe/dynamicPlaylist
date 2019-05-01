48dc0ea69418d31e800961d1983e8a36e143ddab
ositaigwe@pursuit.org <j0h@example.com>
2019-05-01 15:34:00 -0400

    changing playlist object 3

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const playlist = {
       name: "Klingon Battle",
       album: "Star Trek: The Motion Picture",
       composer: ['Jerry Goldsmith'],
-      frame: `<iframe width="200" height="200" src="https://www.youtube-nocookie.com/embed/xan7Rw6KZcM?controls=0&amp;start=269" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+      frame: `<iframe width="560" height="315" src="https://www.youtube.com/embed/XjlBXmn2Q0s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
       info: "Label: Columbia/Legacy, Original Copyright: 1979 (All Rights Reserved)"
     },
     {
